9547d1596520fd5a0e23e74847ba0a27a3ce2112
Ryan Booth <ryanboothforever@gmail.com>
2021-01-26 20:42:33 -0600

    Created cal design & started making object

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+function Calculator(){
+    this.addToDisplay = function (e) {
+        //When a user clicks on a number, the number appears in the h2
+        let outcome = document.querySelector("#beep");
+        let itemClicked = e.path[0].innerHTML;
+        if (itemClicked <= 9) {
+            let itemNumber = Number(itemClicked);
+            outcome.innerHTML += itemNumber;
+        } else if (itemClicked === "/" || itemClicked === "*" || itemClicked === "-" || itemClicked === "+") {
+            outcome.innerHTML += " " + itemClicked + " ";
+        }
+        let myParts = outcome.innerHTML.split(" ")
+        return myParts;
+    }
+
+    this.operators = function(){
+        const opSign = document.querySelectorAll(".operator");
+        Array.from(opSign).forEach(elements => elements.addEventListener("click", addToDisplay))
+
+        const finalResult = document.querySelector(".equal-sign");
+        finalResult.addEventListener("click", tallyResult)
+    }
+
+    this.tally = function(){
+        let outcome = document.querySelector("#beep");
+        let myParts = outcome.innerHTML.split(" ")
+        let result;
+        if (myParts[1] === "+") {
+            result = Number(myParts[0]) + Number(myParts[2])
+        } else if (myParts[1] === "-") {
+            result = Number(myParts[0]) - Number(myParts[2])
+        } else if (myParts[1] === "*") {
+            result = Number(myParts[0]) * Number(myParts[2])
+        } else if (myParts[1] === "/") {
+            result = Number(myParts[0]) / Number(myParts[2])
+        } else {
+            return false
+        }
+        outcome.innerHTML = result
+    }
+    this.clearDisplay = function(){
+        document.querySelector("#beep").innerHTML = ""
+    }
+    const numbers = document.getElementsByTagName("span");
+    Array.from(numbers).forEach(elements => elements.addEventListener("click", addToDisplay))
+
+    document.querySelector("#clear").addEventListener("click", clearDisplay)
+}
+// const numbers = document.getElementsByTagName("span");
+// Array.from(numbers).forEach(elements => elements.addEventListener("click", addToDisplay))
+
+//When user clicks an operator, the operator appears in the h2
+
+// const opSign = document.querySelectorAll(".operator");
+// Array.from(opSign).forEach(elements => elements.addEventListener("click", addToDisplay))
+
+// const finalResult = document.querySelector(".equal-sign");
+// finalResult.addEventListener("click", tallyResult)
+
+// function addToDisplay(e) {
+//     let outcome = document.querySelector("#beep");
+//     let itemClicked = e.path[0].innerHTML;
+//     if (itemClicked <= 9) {
+//         let itemNumber = Number(itemClicked);
+//         outcome.innerHTML += itemNumber;
+//     } else if (itemClicked === "/" || itemClicked === "*" || itemClicked === "-" || itemClicked === "+") {
+//         outcome.innerHTML += " " + itemClicked + " ";
+//     }
+//     let myParts = outcome.innerHTML.split(" ")
+//     return myParts;
+
+// }
+
+//When user clicks the "=", do math of what is in the h2, and then re assign the h2 to that new value.
+
+// function tallyResult() {
+//     let outcome = document.querySelector("#beep");
+//     let myParts = outcome.innerHTML.split(" ")
+//     let result;
+//     if (myParts[1] === "+") {
+//         result = Number(myParts[0]) + Number(myParts[2])
+//     } else if (myParts[1] === "-") {
+//         result = Number(myParts[0]) - Number(myParts[2])
+//     } else if (myParts[1] === "*") {
+//         result = Number(myParts[0]) * Number(myParts[2])
+//     } else if (myParts[1] === "/") {
+//         result = Number(myParts[0]) / Number(myParts[2])
+//     } else {
+//         return false
+//     }
+//     outcome.innerHTML = result
+// }
+
+//Clear button clears the display 
+
+// document.querySelector("#clear").addEventListener("click", clearDisplay)
+
+// function clearDisplay() {
+//     document.querySelector("#beep").innerHTML = ""
+// }
